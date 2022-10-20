27725027c80e1da81f4983414ef4834d52711c44
Elias Meisl <elias.meisl@stud.fh-campswien.ac.at>
2022-10-20 18:09:27 +0200

    task one done

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,11 +1,31 @@
 package at.ac.fhcampuswien;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
 public class App {
 
-    // Implement all methods as public static
+    public static void oneMonthCalendar(int days, int firstDay) {
+
+    int daysPerWeek = firstDay - 1;
+    int space;
+    int calendarDays;
+
+    for (space = 1; space <= 3*(firstDay-1); space++) {
+        System.out.print(" ");
+        }
+
+    for (calendarDays =1; calendarDays <=days; calendarDays++) {
+        System.out.printf("%2d", calendarDays);
+        System.out.print(" ");
+        daysPerWeek++;
+        if (daysPerWeek == 7) {
+            System.out.println();
+            daysPerWeek = 0;
+        }
+    }
+    }
 
     public static void main(String[] args) {
         // test your method implementations here
