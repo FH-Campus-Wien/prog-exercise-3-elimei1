059601dd6c5ea32a35de227387e7b976ceb0dbc1
Elias Meisl <elias.meisl@stud.fh-campswien.ac.at>
2022-10-21 14:45:08 +0200

    task 4 done

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -27,6 +27,69 @@ public class App {
     }
     }
 
+    public static long[] lcg(long seed) {
+
+        long [] tenRandomNumbers = new long[10];
+        double m = Math.pow(2, 31);
+        int a = 1103515245;
+        int c = 12345;
+        long x1 =0;
+
+        for (int i = 0; i < 10; i++) {
+            x1 =  ( (long) a * seed + (long) c) % (long) m;
+            seed = x1;
+            tenRandomNumbers [i]= x1;
+        }
+        return tenRandomNumbers;
+    }
+
+    public static void guessingGame(int numberToGuess) {
+
+    Scanner scanner = new Scanner(System.in);
+    int i;
+
+    for (i = 1; i < 11; i++){
+        System.out.print("Guess number " + i + ": ");
+        int guess = scanner.nextInt();
+        if (i == 10) {
+            System.out.println("You lost! Have you ever heard of divide & conquer?");
+            break;
+        }
+        else if (guess == numberToGuess) {
+            System.out.println("You won wisenheimer!");
+            break;
+        }
+        else if (guess < numberToGuess) {
+            System.out.println("The number AI picked is higher than your guess.");
+        }
+        else {
+            System.out.println("The number AI picked is lower than your guess.");
+        }
+    }
+    }
+
+    public static int randomNumberBetweenOneAndHundred() {
+
+        Random r = new Random();
+        return (r.nextInt(100) + 1);
+    }
+
+    public static boolean swapArrays(int[] arrayOne, int[] arrayTwo) {
+
+        if (arrayOne.length != arrayTwo.length) {
+            return false;
+        }
+
+        for (int i = 0; i < arrayOne.length; i++) {
+            arrayOne[i] = arrayOne[i] + arrayTwo[i];
+            arrayTwo[i] = arrayOne[i] - arrayTwo[i];
+            arrayOne[i] = arrayOne[i] - arrayTwo[i];
+        }
+        return true;
+    }
+
+
+
     public static void main(String[] args) {
         // test your method implementations here
         // make method calls
